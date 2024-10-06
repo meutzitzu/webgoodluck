d7b182ce0fd7dfbf401bb97b56799e68bfd0843d
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-10-06 18:52:23 +0300

    speeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed

--- webgl.js
+++ webgl.js
@@ -1,5 +1,5 @@
 "use strict";
-let x=0.0, y=0.0, z=0.1, r=0.0;
+let x=0.0, y=0.0, z=1.0, r=0.0, speed=0.03;
 
 var pressedKeys = {};
 window.onkeyup = function(e) { pressedKeys[e.keyCode] = false; }
@@ -129,12 +129,12 @@ function main() {
 			
       //recursive call to renderLoop
 			window.requestAnimationFrame(renderLoop);
-				z*=(pressedKeys[16] ? 1.1 : 1.0);
-				z*=(pressedKeys[17] ? 0.9 : 1.0);
-				y+=z*(pressedKeys[87] ? 0.1 : 0.0);
-				y+=z*(pressedKeys[83] ? -0.1 : 0.0);
-				x+=z*(pressedKeys[65] ? -0.1 : 0.0);
-				x+=z*(pressedKeys[68] ? 0.1 : 0.0);
+				z*=(pressedKeys[16] ? 1/(1.0+speed) : 1.0);
+				z*=(pressedKeys[17] ? 1.0+speed : 1.0);
+				y+=z*(pressedKeys[87] ? speed : 0.0);
+				y+=z*(pressedKeys[83] ? -speed : 0.0);
+				x+=z*(pressedKeys[65] ? -speed : 0.0);
+				x+=z*(pressedKeys[68] ? speed : 0.0);
 			}
 
 			// begin the render loop
